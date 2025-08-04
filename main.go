e319efbb3dae8ad130b6d9ffe7c80db6c76246fa
Dalton Williams <email@dalton.dog>
2025-08-04 18:47:14 -0500

    Moved cmd stuff directly into main

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "go.dalton.dog/campfire/cmd"
+import (
+	"os"
 
+	"go.dalton.dog/campfire/models"
+
+	tea "github.com/charmbracelet/bubbletea/v2"
+	"github.com/charmbracelet/log"
+)
+
+// TODO: Consider implementing cobra, but not sure this is complex enough to warrant that
+
+// Entry point of the program, starts up the BubbleTea program
 func main() {
-	cmd.Run()
+	if len(os.Args) < 2 {
+		log.Info("Usage: campfire <path/to/logfile>")
+		return
+	}
+
+	logfilePath := os.Args[1]
+
+	model := models.NewModel(logfilePath)
+
+	p := tea.NewProgram(
+		model,
+		tea.WithAltScreen(),       // Use the full size of the terminal
+		tea.WithMouseCellMotion(), // Enable tracking the mouse wheel
+	)
+
+	if _, err := p.Run(); err != nil {
+		log.Fatalf("Error running program:\n%v", err)
+	}
 }
